a33d0e46f90f5daca6d8e11c3b1622f65a9ce282
bpuibusque <baptiste.puibusque@limayrac.fr>
2025-02-21 16:19:32 +0100

    push

--- frais/src/main/java/fr/limayrac/frais/service/DeclarationFraisService.java
+++ frais/src/main/java/fr/limayrac/frais/service/DeclarationFraisService.java
@@ -1,7 +1,11 @@
 package fr.limayrac.frais.service;
 
+import fr.limayrac.frais.model.CoordonneesBancaires;
 import fr.limayrac.frais.model.DeclarationFrais;
+import fr.limayrac.frais.model.FraisHebergement;
+import fr.limayrac.frais.model.FraisRestauration;
 import fr.limayrac.frais.model.DeclarationFrais.StatutDeclaration;
+import fr.limayrac.frais.model.FraisTransport;
 import fr.limayrac.frais.repository.DeclarationFraisRepository;
 import org.springframework.stereotype.Service;
 
@@ -43,10 +47,28 @@ public class DeclarationFraisService {
     public DeclarationFrais initDeclaration(String professeur) {
         DeclarationFrais declaration = new DeclarationFrais();
         declaration.setProfesseur(professeur);
-        declaration.setStatut(DeclarationFrais.StatutDeclaration.EN_ATTENTE);
         declaration.setDate(LocalDate.now());
+        declaration.setStatut(DeclarationFrais.StatutDeclaration.EN_ATTENTE);
+
+        FraisTransport fraisTransport = new FraisTransport();
+        fraisTransport.setDeclarationFrais(declaration);
+        declaration.setFraisTransport(fraisTransport);
+        
+        FraisHebergement fraisHebergement = new FraisHebergement();
+        fraisHebergement.setDeclarationFrais(declaration);
+        declaration.setFraisHebergement(fraisHebergement);
+
+        FraisRestauration fraisRestauration = new FraisRestauration();
+        fraisRestauration.setDeclarationFrais(declaration);
+        declaration.setFraisRestauration(fraisRestauration);
+
+        CoordonneesBancaires coordonneesBancaires = new CoordonneesBancaires();
+        coordonneesBancaires.setDeclarationFrais(declaration);
+        declaration.setCoordonneesBancaires(coordonneesBancaires);
+
         return declarationFraisRepository.save(declaration);
     }
+
 
     public void soumettreDeclaration(DeclarationFrais declaration) {
         declaration.setStatut(DeclarationFrais.StatutDeclaration.EN_ATTENTE);
